07a493950e4676554b10457497abef0ba987f9cf
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2022-05-06 19:49:58 +0300

    Error

--- view/error404.php
+++ view/error404.php
@@ -1,7 +1,12 @@
-<?php
-ob_start();
+<?php ob_start(); 
 ?>
-Error 404
+<div class="center">
+<h2>404 Error  </h2>
+<article>
+    <h4 class="h4">	Page not found for the requested URL</h4>
+
+</article>
+</div>
 <?php
 $content = ob_get_clean();
 include_once 'view/layout.php';
